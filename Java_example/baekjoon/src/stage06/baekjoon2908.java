c4826a16eeee26d9a45c4edea0b22e3a79671a3c
HeoJiye <78946499+HeoJiye@users.noreply.github.com>
2022-07-12 17:20:23 +0900

    2908 풀이

--- Java_example/baekjoon/src/stage06/baekjoon2908.java
+++ Java_example/baekjoon/src/stage06/baekjoon2908.java
@@ -1,10 +1,28 @@
 package stage06;
 
+import java.util.Scanner;
+
 public class baekjoon2908 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int n = filpN(N);
+		int m = filpN(M);
+		
+		System.out.println(n > m ? n : m);
 	}
-
+	
+	static int filpN(int N) {
+		int n = 0;
+		while(N > 0) {
+			n *= 10;
+			n += N % 10;
+			N /= 10;
+		}
+		return n;
+	}
 }
